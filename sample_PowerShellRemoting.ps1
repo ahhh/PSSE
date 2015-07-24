36bc59fa673302c7f57b1fcacde03fb8b4903e27
ahhh <ahhh.db@gmail.com>
2015-07-24 03:38:30 -0700

    Update sample_PowerShellRemoting.ps1

--- sample_PowerShellRemoting.ps1
+++ sample_PowerShellRemoting.ps1
@@ -1,17 +1,31 @@
-# PowerShell script for enabling (and disabling) PowerShell Remoting
+## PowerShell script for enabling (and disabling) PowerShell Remoting
 
-# check it
+#$COMPUTER = "This machine's fqdn, who whill be executing the PowerShell remoting commands"
+#$USERNAME = "Username on $COMPUTER executing the remote commands"
+#$REMOTECOMPUTER = "The machine's fqdn, who issuing the remote commands "
+
+## check WinRM state
 get-service winrm
 
-# Enable it
+## Enable WinRM
 Enable-PSRemoting –force
-# quick config
+## quick config
 winrm quickconfig
-# add trusted host
-winrm s winrm/config/client '@{TrustedHosts="RemoteComputer"}'
+## add specific trusted host
+#winrm s winrm/config/client '@{TrustedHosts="REMOTECOMPUTER"}'
+## or whitelist all as trusted hosts
+#Set-Item wsman:\localhost\client\trustedhosts *
+## restart WinRM
+restart-service WinRM
 
-# Disable it
+## Disable WinRM
 #Disable-PSRemoting -force
 #Stop-Service winrm
 #Set-Service -Name winrm -StartupType Disabled
 #Set-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name LocalAccountTokenFilterPolicy -Value 0 -Type DWord
+
+## Access this machine from a remote machine with:
+#Test-WsMan $COMPUTER
+#Enter-PSSession -ComputerName "COMPUTER" -credential "USERNAME"
+## Run a remote command
+#Invoke-Command -ComputerName $COMPUTER -ScriptBlock { COMMAND } -credential $USERNAME
