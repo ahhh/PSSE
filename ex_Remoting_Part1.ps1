a5f893b0731ca481b502d50670f008432a186af9
ahhh <ahhh.db@gmail.com>
2016-03-06 10:23:01 -0800

    Update ex_Remoting_Part1.ps1

--- ex_Remoting_Part1.ps1
+++ ex_Remoting_Part1.ps1
@@ -1,2 +1,3 @@
-﻿#Replace Computername and Credential with the ones in your lab
+# Example by Nikhil Mittal : http://www.labofapenetrationtester.com/
+# Replace Computername and Credential with the ones in your lab
 Get-HotFix -Id KB2871997 -ComputerName domainpc -Credential example\domainuser
