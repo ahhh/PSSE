ae801d189fe6511567d43eba80fabce26eeff074
ahhh <ahhh.db@gmail.com>
2016-03-06 10:22:31 -0800

    Update ex_Registry_Part3.ps1

--- ex_Registry_Part3.ps1
+++ ex_Registry_Part3.ps1
@@ -1,3 +1,4 @@
-﻿#Make the fucntion call to Check-Macrosecurity from the script Registry-Part2.ps1
+# Example by Nikhil Mittal : http://www.labofapenetrationtester.com/
+# Make the fucntion call to Check-Macrosecurity from the script Registry-Part2.ps1
 
 Invoke-Command -FilePath .\Registry_Part2.ps1 -ComputerName domainpc -Credential bharat\domainuser
