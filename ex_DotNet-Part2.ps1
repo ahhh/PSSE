17fa15e54caa0a31c8279f2cf9dd192eefad4654
ahhh <ahhh.db@gmail.com>
2016-03-06 10:20:05 -0800

    Update ex_DotNet-Part2.ps1

--- ex_DotNet-Part2.ps1
+++ ex_DotNet-Part2.ps1
@@ -1,3 +1,4 @@
-﻿$DNSClass = [AppDomain]::CurrentDomain.GetAssemblies() | ForEach-Object {$_.GetTypes()}| where {$_.IsPublic -eq "True"} | where {$_.Name -eq "DNS"}
+# Example by Nikhil Mittal : http://www.labofapenetrationtester.com/
+$DNSClass = [AppDomain]::CurrentDomain.GetAssemblies() | ForEach-Object {$_.GetTypes()}| where {$_.IsPublic -eq "True"} | where {$_.Name -eq "DNS"}
 $DNSClass | Get-Member -Static
 $DNSClass::GetHostByName("labofapenetrationtester.com")
