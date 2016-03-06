60f9c3195abff3c3528b27df6b4581bb4c082cdb
ahhh <ahhh.db@gmail.com>
2016-03-06 10:20:17 -0800

    Update ex_Enable_Scripting.ps1

--- ex_Enable_Scripting.ps1
+++ ex_Enable_Scripting.ps1
@@ -1,3 +1,4 @@
+# Example by Nikhil Mittal : http://www.labofapenetrationtester.com/
 # Enable running any PowerShell scripts, normally / default they are restricted
 #powershell -ExecutionPolicy ByPass -File ./example.ps1
 Set-ExecutionPolicy Unrestricted
