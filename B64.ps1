5d33317e51bb9584f907be8ee8d41346edf9179e
ahhh <ahhh.db@gmail.com>
2016-04-20 11:10:40 -0700

    Update B64.ps1

--- B64.ps1
+++ B64.ps1
@@ -16,6 +16,7 @@ PS C:\> Powershell.exe -Enc bgBlAHQAIAB1AHMAZQByAA==
 .LINK
 https://github.com/ahhh/
 http://lockboxx.blogspot.com/
+https://blogs.msdn.microsoft.com/timid/2014/03/26/powershell-encodedcommand-and-round-trips/
 .NOTES
 EZ-Mode tool
 #>
@@ -29,10 +30,45 @@ EZ-Mode tool
 	
 	)
 
-    # To use the -EncodedCommand parameter:
-    $command = $Commandz
-    $bytes = [System.Text.Encoding]::Unicode.GetBytes($command)
-    $encodedCommand = [Convert]::ToBase64String($bytes)
-    Write-Host $encodedCommand
+	# To use the -EncodedCommand parameter:
+	$command = $Commandz
+	$bytes = [System.Text.Encoding]::Unicode.GetBytes($command)
+	$encodedCommand = [Convert]::ToBase64String($bytes)
+	Write-Host $encodedCommand
+    
+}
+
+function Decode-Command
+{
+
+<#
+.SYNOPSIS
+PowerShell cmdlet for b64 encoding strings
+.DESCRIPTION
+this script is for decoding b64 commands that get run in PowerShell -Enc
+.PARAMETER command
+This is the string you want to base 64 decode
+.EXAMPLE
+PS C:\> Import-Module B64.ps1
+PS C:\> Decode-Command -c bgBlAHQAIAB1AHMAZQByAA==
+.LINK
+https://github.com/ahhh/
+http://lockboxx.blogspot.com/
+https://blogs.msdn.microsoft.com/timid/2014/03/26/powershell-encodedcommand-and-round-trips/
+.NOTES
+EZ-Mode tool
+#>
+
+	[CmdletBinding()] Param(
+	
+		[Parameter(Mandatory = $true, ValueFromPipeline=$true)]
+		[Alias("c", "command")]
+		[String]
+		$Commandz
+	
+	)
+
+	$decodedCommand = [System.Text.Encoding]::Unicode.GetString([System.Convert]::FromBase64String($Commandz));
+	Write-Host $decodedCommand
     
 }
